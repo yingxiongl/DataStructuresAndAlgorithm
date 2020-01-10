bfceb5b9bdb71b1007d516a42b7b47a21eac2fcd
yingxiongl <2065137006@qq.com>
2020-01-10 10:52:44 +0800

    扑克牌

--- 扑克牌/扑克牌/PaperCard.cs
+++ 扑克牌/扑克牌/PaperCard.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace 扑克牌
 {
-    class PaperCard
+    public class PaperCard
     {
+        public string Type { get; set; }
+        public string Number { get; set; }
+        public PaperCard(string strType, string strNumber)
+        {
+            this.Type = strType;
+            this.Number = strNumber;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0}{1}", this.Type, this.Number);
+            }
+
+            private set { }
+        }
     }
 }
